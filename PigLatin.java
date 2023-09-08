ed0c578ceb1d3411849dff2db70062156c5abf5f
Jasper Burroughs <burrojas26@7hills.org>
2023-09-08 13:13:29 -0400

    Solving Errors

--- PigLatin.java
+++ PigLatin.java
@@ -14,15 +14,17 @@ public class PigLatin {
         for (int i = 0; i < len; i++) {
             if (origPhrase.charAt(i) == ' ') {
                 phrase = phrase.substring(0, i);
-                if (phrase.charAt(0) == 'a' || phrase.charAt(0) == 'e' || phrase.charAt(0) == 'i' || phrase.charAt(0) == 'o' || phrase.charAt(0) == 'u') {
+                System.out.println(phrase);
+                //if (phrase.charAt(0) == 'a' || phrase.charAt(0) == 'e' || phrase.charAt(0) == 'i' || phrase.charAt(0) == 'o' || phrase.charAt(0) == 'u') {
                     edited = phrase + "way";
-                }
-                else {
-                    edited = phrase.substring(1, len) + phrase.substring(0, 1) + "ay";
-                }
-                System.out.println(edited);
+                //}
+                //else {
+                    //edited = phrase.substring(1, len) + phrase.substring(0, 1) + "ay";
+                //}
+                
             }
         }
+        System.out.println(edited);
         
     }
 }
