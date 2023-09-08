83f14e7c62bb8217c61e2d0db5c409619cf3c9f8
Jasper Burroughs <burrojas26@7hills.org>
2023-09-08 13:08:44 -0400

    Added for loop

--- PigLatin.java
+++ PigLatin.java
@@ -9,16 +9,20 @@ public class PigLatin {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter phrase: ");
         phrase = scan.nextLine().toLowerCase();
+        String origPhrase = phrase;
         int len = phrase.length();
         for (int i = 0; i < len; i++) {
-            if (phrase.charAt(i) == ' ')
+            if (origPhrase.charAt(i) == ' ') {
+                phrase = phrase.substring(0, i);
+                if (phrase.charAt(0) == 'a' || phrase.charAt(0) == 'e' || phrase.charAt(0) == 'i' || phrase.charAt(0) == 'o' || phrase.charAt(0) == 'u') {
+                    edited = phrase + "way";
+                }
+                else {
+                    edited = phrase.substring(1, len) + phrase.substring(0, 1) + "ay";
+                }
+                System.out.println(edited);
+            }
         }
-        if (phrase.charAt(0) == 'a' || phrase.charAt(0) == 'e' || phrase.charAt(0) == 'i' || phrase.charAt(0) == 'o' || phrase.charAt(0) == 'u') {
-            edited = phrase + "way";
-        }
-        else {
-            edited = phrase.substring(1, len) + phrase.substring(0, 1) + "ay";
-        }
-        System.out.println(edited);
+        
     }
 }
