48e4c9e578fdec6cc8a18ed453d0a697fb7e9939
Jasper Burroughs <jasperjames8@icloud.com>
2023-09-13 19:20:13 -0400

    Fixed the variables so that it can translate a phrase

--- PigLatin.java
+++ PigLatin.java
@@ -14,23 +14,25 @@ public class PigLatin {
         for (int i = 0; i < len; i++) {
             if (origPhrase.charAt(i) == ' ') {
                 phrase = origPhrase.substring(0, i);
+                System.out.println(phrase);
                 origPhrase = origPhrase.substring(i+1, origPhrase.length());
                 if (phrase.charAt(0) == 'a' || phrase.charAt(0) == 'e' || phrase.charAt(0) == 'i' || phrase.charAt(0) == 'o' || phrase.charAt(0) == 'u') {
-                    edited = phrase + "way ";
+                    edited += phrase + "way ";
                 }
                 else {
-                    edited = phrase.substring(1, phrase.length()) + phrase.substring(0, 1) + "ay ";
+                    edited += phrase.substring(1, phrase.length()) + phrase.substring(0, 1) + "ay ";
                 }
-                len = phrase.length();
+                len = origPhrase.length();
                 i = 0;
             }
             
         }
         if (origPhrase.charAt(0) == 'a' || origPhrase.charAt(0) == 'e' || origPhrase.charAt(0) == 'i' || origPhrase.charAt(0) == 'o' || origPhrase.charAt(0) == 'u') {
-            edited += phrase + "way ";
+            edited += origPhrase + "way ";
         }
         else {
-            edited += origPhrase.substring(1, origPhrase.length()) + origPhrase.substring(0, 1) + "ay";
+            edited += origPhrase.substring(1, origPhrase.length()) + origPhrase.substring(0, 1) + "ay ";
+            System.out.println(edited);
         }
         System.out.println(edited);
         
